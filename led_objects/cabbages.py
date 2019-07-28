99bd55e129ffd62377ba2826c08e9c544a91c80e
amir <amirgiraffe@gmail.com>
2019-07-29 00:23:06 +0300

    add cabbage object

--- led_objects/cabbages.py
+++ led_objects/cabbages.py
@@ -1,18 +1,32 @@
+from led_objects.led_object import LedObject
 
 
-class Cabbage1:
-    name = "cabbage-1"
-    total_pixels = 260
-    mapping = {
-        "rand": list(range(total_pixels))
-    }
+class Cabbage(LedObject):
+
+    def __init__(self, num_pixels):
+        super(Cabbage, self).__init__(total_pixels=num_pixels)
 
     @classmethod
     def default_mapping(cls):
-        return "rand"
+        return "a"
 
-    animations = []
-    @classmethod
-    def add_animation(cls, animation):
-        animation.set_pixels(cls.default_mapping())
-        cls.animations.append(animation)
+
+cabbage1 = Cabbage(300)
+
+
+# class Cabbage1:
+#     name = "cabbage-1"
+#     total_pixels = 260
+#     mapping = {
+#         "rand": list(range(total_pixels))
+#     }
+#
+#     @classmethod
+#     def default_mapping(cls):
+#         return "rand"
+#
+#     animations = []
+#     @classmethod
+#     def add_animation(cls, animation):
+#         animation.set_pixels(cls.default_mapping())
+#         cls.animations.append(animation)
