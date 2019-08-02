8db9d11dc160337b0ad070a20a51a8e7cfd71d88
tehila <tehilaguy88@gmail.com>
2019-08-02 18:55:41 +0300

    add colors

--- infra/colors.py
+++ infra/colors.py
@@ -1,13 +1,26 @@
 
-red = (0.0 / 3.0, 1.0, 1.0)
+red = (0.995, 1.0, 1.0)
 green = (1.0 / 3.0, 1.0, 1.0)
-blue = (2.0 / 3.0, 1.0, 1.0)
+blue = (0.61, 1.0, 1.0)
 
-
-pink = (0.95, 0.75, 1.0)
+pink_string = (0.88, 0.7, 1.0)
+pink_strip = (0.98, 0.75, 1.0)
 
 coral = (0.99, 0.85, 1.0)
 
-light_green = (0.33, 0.9, 1.0)
+light_green = (0.315, 0.9, 1.0)
 
 
+yellow_string = (0.14, 0.95, 1.0)
+yellow_strip = (0.11, 0.95, 1.0)
+
+orange_string = (0.065, 1.0, 1.0)
+orange_strip = (0.045, 1.0, 1.0)
+
+turquoise_string = (0.46, 0.95, 1.0)
+turquoise_strip = (0.47, 0.95, 1.0)
+
+purple_string = (0.7, 1.0, 1.0)
+purple_strip = (0.72, 1.0, 1.0)
+
+magenta = (0.85, 1.0, 1.0)
