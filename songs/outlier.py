15b865814e0d09a34289526148d4af2a0767a862
Amir Blum <amirgiraffe@gmail.com>
2019-08-07 00:05:03 +0300

    add new sequence files for songs

---
+++ songs/outlier.py
@@ -0,0 +1,21 @@
+from animations import brightness
+from infra.animations_factory import color, effect
+from infra.length import short, medium, long, soft, hard, total
+from infra.stored_animations import save, beat, load
+from led_objects.cabbages import cabbage1, cabbage6, brain7, brain4, cabbage5, cabbages, brains, donut1, donut3
+from led_objects.led_object import all
+from led_objects.flowers import flower6, flowers, paper5
+from led_objects.objects_selector import elements
+from led_objects.stands import sticks8, single_sticks, sticks7, sticks3, lifas5, lifas1, lifas4
+from network.send_to_mqtt import send_to_mqtt
+from infra.timing import song_settings, episodes, episode, cycle, cycle_beats
+from infra.colors import *
+
+song_settings(bpm=117, beats_per_episode=32)
+
+
+
+send_to_mqtt("outlier")
+
+
+
