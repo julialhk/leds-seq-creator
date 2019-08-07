b289aab47b8b1e989e9b64184817b7bcd9f55991
Shani Sabag <shani.sabag@cappsool.com>
2019-08-07 22:27:09 +0300

    add player scripts

--- songs/millenium.py
+++ songs/millenium.py
@@ -1,16 +1,22 @@
-from infra.animations_factory import color
-from led_objects.flowers import flower6
+from animations import brightness
+from infra.animations_factory import color, effect
+from infra.length import short, medium, long, soft, hard, total
+from infra.stored_animations import save, beat, load
+from led_objects.cabbages import cabbage1, cabbage6, brain7, brain4, cabbage5, cabbages, brains, donut1, donut3
+from led_objects.led_object import all
+from led_objects.flowers import flower6, flowers, paper5
 from led_objects.objects_selector import elements
-from led_objects.stands import sticks8
+from led_objects.stands import sticks8, single_sticks, sticks7, sticks3, lifas5, lifas1, lifas4
 from network.send_to_mqtt import send_to_mqtt
-from infra.timing import song_settings, episodes
+from infra.timing import song_settings, episodes, episode, cycle, cycle_beats
 from infra.colors import *
 
 song_settings(bpm=124, beats_per_episode=32)
 
-episodes(0, 50)
-elements(flower6, sticks8)
-color.alternate(red, blue)
+episodes(0, 30)
+elements(all)
+color.uniform(red)
+effect.breath()
 
 # elements(sticks1)
 # effect.brightness(beats_per_cycle=2, beat_feel=BeatFeel.drama_beat, energy=1.0)
