5e88fc7630781cbead6195948e2049edd3a3be6c
EhudB <blumehud@gmail.com>
2019-08-09 17:09:06 +0300

    Fixed all attributes

--- led_objects/flowers.py
+++ led_objects/flowers.py
@@ -22,19 +22,3 @@ paper2 = Flower(50)
 papers = [paper2, paper5]
 
 
-class Fllod(LedObject):
-
-    def __init__(self, num_pixels):
-        super(Fllod, self).__init__(total_pixels=num_pixels)
-        flowers.append(self)
-
-cup_cake3 = Fllod(100)
-
-cup_cakes = [cup_cake3]
-
-rug6 = Fllod(11)
-rug4 = Fllod(11)
-
-rugs = [rug4, rug6]
-
-floods = [cup_cakes, rugs]
--- thing_to_obj_map.py
+++ thing_to_obj_map.py
@@ -1,5 +1,6 @@
 from led_objects.cabbages import cabbage1, cabbage6, brain7, cabbage5, cup_cake4, donut1, donut3
-from led_objects.flowers import flower6, bottle4, bottle5, paper5, cup_cake3, paper2, flower1, rug6, rug4
+from led_objects.flood import cup_cake3
+from led_objects.flowers import flower6, bottle4, bottle5, paper5, paper2, flower1
 from led_objects.stands import sticks8, sticks7, sticks3, lifas5, lifas1, lifas4
 
 obj_to_thing = {
