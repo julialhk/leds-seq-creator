1bd3ce102f8161b021d40a92dbc2e6c110217323
tehila <tehilaguy88@gmail.com>
2019-08-09 17:36:22 +0300

    fix group names issues

--- songs/essoteric.py
+++ songs/essoteric.py
@@ -2,14 +2,16 @@ from animations import brightness
 from infra.animations_factory import color, effect
 from infra.length import short, medium, long, soft, hard, total
 from infra.stored_animations import save, beat, load
-from led_objects.cabbages import cabbage1, cabbage6, brain7, brain4, cabbage5, cabbages, brains, donut1, donut3
-from led_objects.groups import group4, group2, group6, group1, group8, group3, group7, group5
+from led_objects.cabbages import cabbage1, cabbage6, brain7, cup_cake4, cabbage5, cabbages, donut1, donut3, \
+    brains, twists, donuts
+from led_objects.flood import cup_cakes
+from led_objects.groups import group1, group2, group3, group4, group5, group6, group7, group8
 from led_objects.led_object import all
-from led_objects.flowers import flower6, flowers, paper5
+from led_objects.flowers import flower6, flowers, paper5, papers, bottles
 from led_objects.objects_selector import elements
-from led_objects.stands import sticks8, single_sticks, sticks7, sticks3, lifas5, lifas1, lifas4
+from led_objects.stands import sticks8, single_sticks, sticks7, sticks3, lifas5, lifas1, lifas4, sticks, lifas, stands
 from network.send_to_mqtt import send_to_mqtt
-from infra.timing import song_settings, episodes, episode, cycle, cycle_beats
+from infra.timing import song_settings, episodes, episode, cycle, cycle_beats, beats
 from infra.colors import *
 
 song_settings(bpm=73.5, beats_per_episode=16)
