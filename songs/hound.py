50f7548fec01a6dd715a745c297d5160317ae8f2
Julia Loshak <julia@craft.io>
2019-12-17 23:43:46 +0200

    saving tests

--- songs/hound.py
+++ songs/hound.py
@@ -2,17 +2,39 @@ from animations import brightness
 from infra.animations_factory import color, effect
 from infra.length import short, medium, long, soft, hard, total
 from infra.stored_animations import save, beat, load
-from led_objects.cabbages import cabbage1, cabbage6, brain7, brain4, cabbage5, cabbages, brains, donut1, donut3
-from led_objects.led_object import all
-from led_objects.flowers import flower6, flowers, paper5
-from led_objects.objects_selector import elements
-from led_objects.stands import sticks8, single_sticks, sticks7, sticks3, lifas5, lifas1, lifas4
+from led_objects.she import she
+from led_objects.they import they1
 from network.send_to_mqtt import send_to_mqtt
 from infra.timing import song_settings, episodes, episode, cycle, cycle_beats
+from led_objects.objects_selector import elements
 from infra.colors import *
 
-song_settings(bpm=120, beats_per_episode=32)
+song_settings(bpm=60, beats_per_episode=1)
 
+#heart = [they1.part1(), they2.part4()]
+
+
+episode(0)
+elements(they1.part1())
+color.gradient(blue, red)
+cycle(1)
+effect.snake()
+
+elements(they1.part2())
+color.gradient(blue, red)
+cycle(1)
+effect.snake(tail=1.0, switch_direction=True)
+
+episode(1)
+elements(they1.part3())
+color.gradient(purple_strip, pink_strip)
+cycle(1)
+effect.snake()
+
+elements(they1.part4())
+color.gradient(purple_strip, pink_strip)
+cycle(1)
+effect.snake(tail=1.0, switch_direction=True)
 
 
 send_to_mqtt("hound")
