f3bd487d68efe16d246f7a5557687aee01a7947e
Julia Loshak <julia@craft.io>
2019-12-19 23:51:11 +0200

    New mapping & heart animation

--- led_objects/they.py
+++ led_objects/they.py
@@ -16,15 +16,26 @@ class They(LedObject):
         self.mapping["1"] = list(range(0, 217))
         self.mapping["2"] = list(reversed(range(217, 432)))
 
-        left_top = 117
+        top1 = 117
 
-        self.mapping["1l"] = list(range(0, left_top))
-        self.mapping["1r"] = list(reversed(range(left_top, 217)))
+        self.mapping["1o"] = list(range(0, top1))
+        self.mapping["1i"] = list(reversed(range(top1, 217)))
 
-        right_top = 333
+        top2 = 333
 
-        self.mapping["2l"] = list(range(217, right_top))
-        self.mapping["2r"] = list(reversed(range(right_top, 432)))
+        self.mapping["2o"] = list(range(217, top2))
+        self.mapping["2i"] = list(reversed(range(top2, 432)))
+
+        top_middle1 = 102
+        low_middle1 = 170
+
+        top_middle2 = 290
+        low_middle2 = 390
+
+        self.mapping["h1"] = list(reversed(range(top_middle1, low_middle1)))
+        self.mapping["h2"] = list(reversed(range(top_middle2, low_middle2)))
+        self.mapping["rh"] = self.mapping["h1"]+self.mapping["h2"]
+        random.shuffle(self.mapping["rh"])
 
     def random(self):
         return SegmentProxy(self, "r")
@@ -35,17 +46,26 @@ class They(LedObject):
     def second(self):
         return SegmentProxy(self, "2")
 
-    def first_left(self):
-        return SegmentProxy(self, "1l")
+    def first_out(self):
+        return SegmentProxy(self, "1o")
 
-    def first_right(self):
-        return SegmentProxy(self, "1r")
+    def first_in(self):
+        return SegmentProxy(self, "1i")
 
-    def second_left(self):
-        return SegmentProxy(self, "2l")
+    def second_in(self):
+        return SegmentProxy(self, "2i")
 
-    def second_right(self):
-        return SegmentProxy(self, "2r")
+    def second_out(self):
+        return SegmentProxy(self, "2o")
+
+    def half1_heart(self):
+        return SegmentProxy(self, "h1")
+
+    def half2_heart(self):
+        return SegmentProxy(self, "h2")
+
+    def heart_random(self):
+        return SegmentProxy(self, "rh")
 
 
 they = They()
